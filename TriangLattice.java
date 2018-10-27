7cd6f5a67c1ab9c5229d0ae4692d956d5cfa272b
Studio-Darboux-Carbonnier <43018326+Studio-Darboux-Carbonnier@users.noreply.github.com>
2018-10-27 02:49:15 +0100

    Update TriangLattice.java

--- TriangLattice.java
+++ TriangLattice.java
@@ -39,7 +39,7 @@ public class TriangLattice {
 	public static double ljPot(double scale) {
 		double result = 0;
 		for (int k = 1; k <= 1000; k++) {
-			double potK = LjPotential.lj(k*scale);
+			double potK = LjPotential.lj(sqrt(k)*scale);
 			double degK = (double) degeneracy(k);
 			double termK = potK * degK;
 			result += termK;
